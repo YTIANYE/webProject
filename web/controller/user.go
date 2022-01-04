3525eb5c660d56b65318ec3d8404d8bf1a64e2e2
yty <13293209977@163.com>
2022-01-04 20:57:51 +0800

    获取 注册页面 图片验证码ID

--- web/controller/user.go
+++ web/controller/user.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"webProject/web/utils"
 )
 
+// 获取 session 信息
 func GetSesion(ctx *gin.Context) {
 	// 初始化一个错误返回的 map
 	resp := make(map[string]string)
@@ -13,4 +15,11 @@ func GetSesion(ctx *gin.Context) {
 	resp["errmsg"] = utils.RecodeText(utils.RECODE_SESSIONERR)
 
 	ctx.JSON(http.StatusOK, resp)
+}
+
+// 获取图片信息
+func GetImageCd(ctx *gin.Context) {
+	uuid := ctx.Param("uuid")
+	fmt.Println("uuid:", uuid)
+
 }
--- web/main.go
+++ web/main.go
@@ -16,8 +16,11 @@ func main() {
 	})*/
 
 	// router.Static("/", "web/view")                        //加载静态资源
-	router.Static("/home", "web/view")                    //加载静态资源
+	router.Static("/home", "web/view") //加载静态资源
+	// 处理 Session
 	router.GET("/api/v1.0/session", controller.GetSesion) //回调函数这里只放函数名就可以了
+	//
+	router.GET("/api/v1.0/imagecode/:uuid", controller.GetImageCd)
 
 	// 启动运行
 	router.Run(":8080")
