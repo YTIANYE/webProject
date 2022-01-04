0d950f71e46d6c0370dbdc9b04edca4ec0cb505f
yty <13293209977@163.com>
2022-01-04 14:05:32 +0800

    基于gin搭建起一个web应用

---
+++ web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// 添加gin框架开发3步骤
+func main() {
+	// 初始化路由
+	router := gin.Default()
+	// 路由匹配
+	router.GET("/", func(context *gin.Context) {
+		context.Writer.WriteString("项目开始了......")
+	})
+	// 启动运行
+	router.Run(":8080")
+
+}
