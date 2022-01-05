1e994fbc6bd7e5010e0899aa53354fc6a644e011
yty <13293209977@163.com>
2022-01-05 22:09:05 +0800

    修改服务端 handler/getCaptcha.go 文件

--- service/getCaptcha/handler/getCaptcha.go
+++ service/getCaptcha/handler/getCaptcha.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/afocus/captcha"
 
-	"github.com/micro/go-micro/util/log"
+	"image/color"
 
 	getCaptcha "webProject/service/getCaptcha/proto/getCaptcha"
 )
@@ -12,37 +15,38 @@ type GetCaptcha struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
 func (e *GetCaptcha) Call(ctx context.Context, req *getCaptcha.Request, rsp *getCaptcha.Response) error {
-	log.Log("Received GetCaptcha.Call request")
-	rsp.Msg = "Hello " + req.Name
-	return nil
-}
 
-// Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *GetCaptcha) Stream(ctx context.Context, req *getCaptcha.StreamingRequest, stream getCaptcha.GetCaptcha_StreamStream) error {
-	log.Logf("Received GetCaptcha.Stream request with count: %d", req.Count)
+	// 生成图片验证码
 
-	for i := 0; i < int(req.Count); i++ {
-		log.Logf("Responding: %d", i)
-		if err := stream.Send(&getCaptcha.StreamingResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
+	// 初始化对象
+	cap := captcha.New()
+
+	// 设置字体
+	if err := cap.SetFont("service/getCaptcha/conf/comic.ttf"); err != nil { //注意设置路径
+		panic(err.Error())
 	}
 
-	return nil
-}
+	// 设置验证码大小
+	cap.SetSize(128, 64)
 
-// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *GetCaptcha) PingPong(ctx context.Context, stream getCaptcha.GetCaptcha_PingPongStream) error {
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		log.Logf("Got ping %v", req.Stroke)
-		if err := stream.Send(&getCaptcha.Pong{Stroke: req.Stroke}); err != nil {
-			return err
-		}
-	}
+	// 设置干扰强度
+	cap.SetDisturbance(captcha.NORMAL)
+
+	// 设置前景色
+	cap.SetFrontColor(color.RGBA{161, 47, 47, 255})
+
+	// 设置背景色
+	cap.SetBkgColor(color.RGBA{137, 190, 178, 128}, color.RGBA{69, 137, 148, 128})
+
+	// 生成字体
+	img, str := cap.Create(4, captcha.NUM)
+	fmt.Println("生成的验证码 str:", str)
+
+	// 将生成的图片序列化
+	imgBuf, _ := json.Marshal(img)
+
+	// 将 imgBuf 使用参数 rsp 传出
+	rsp.Img = imgBuf
+
+	return nil
 }
