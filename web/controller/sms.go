26acb1c311b3fa7b3af6a5d70328e5142c5cefda
yty <13293209977@163.com>
2022-01-07 22:05:06 +0800

    发送短信验证码，并反馈给前端

---
+++ web/controller/sms.go
@@ -0,0 +1,32 @@
+// This file is auto-generated, don't edit it. Thanks.
+package controller
+
+import (
+	"github.com/alibabacloud-go/tea/tea"
+)
+
+import (
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v2/client"
+)
+
+/**
+ * 使用AK&SK初始化账号Client
+ * @param accessKeyId
+ * @param accessKeySecret
+ * @return Client
+ * @throws Exception
+ */
+func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *dysmsapi20170525.Client, _err error) {
+	config := &openapi.Config{
+		// 您的AccessKey ID
+		AccessKeyId: accessKeyId,
+		// 您的AccessKey Secret
+		AccessKeySecret: accessKeySecret,
+	}
+	// 访问的域名
+	config.Endpoint = tea.String("dysmsapi.aliyuncs.com")
+	_result = &dysmsapi20170525.Client{}
+	_result, _err = dysmsapi20170525.NewClient(config)
+	return _result, _err
+}
--- web/controller/user.go
+++ web/controller/user.go
@@ -5,11 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/afocus/captcha"
+	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v2/client"
+	"github.com/alibabacloud-go/tea/tea"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro"
 	"github.com/micro/go-plugins/registry/consul"
 	"image/png"
+	"math/rand"
 	"net/http"
+	"time"
+	"webProject/web/model"
 	"webProject/web/proto/getCaptcha"
 	"webProject/web/utils"
 )
@@ -64,11 +69,54 @@ func GetSmscd(ctx *gin.Context) {
 	// 拆分 GET 请求中的 URL   === 格式： 资源路径 ？key=value&key=value&key=value
 	imgCode := ctx.Query("text")
 	uuid := ctx.Query("id")
-
 	fmt.Println("---out---:", "phone:", phone, "imgCode:", imgCode, "uuid", uuid)
 
-	// 校验图片验证码
+	// 创建一个容器， 存储返回的数据信息
+	resp := make(map[string]string)
 
-	// 发送短信
+	// 校验图片验证码 是否正确
+	result := model.CheckImgCode(uuid, imgCode)
+	if result {
+		// 发送短信
+		client, _err := CreateClient(tea.String("LTAI5tNjSPYJNT4wyTbWzYLr"), tea.String("B3R0SpewZr83NCyDVuiPlPDQwk4hYj"))
+		if _err != nil {
+			fmt.Println("发送短信错误 _err", _err)
+		}
+
+		// 生成一个随机的六位数，作为验证码
+		rand.Seed(time.Now().UnixNano()) //播种随机种子
+		// 生成6位随机数
+		smsCode := fmt.Sprintf("%06d", rand.Int31n(1000000))
+		smsCodeString := "{" + "\"code\":" + "\"" + smsCode + "\"" + "}"
+
+		sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
+			SignName:      tea.String("阿里云短信测试"),
+			TemplateCode:  tea.String("SMS_154950909"),
+			PhoneNumbers:  tea.String(phone),
+			TemplateParam: tea.String(smsCodeString),
+		}
+
+		// 打印 API 的返回值
+		_, _err = client.SendSms(sendSmsRequest)
+		if _err != nil {
+			resp["errno"] = utils.RECODE_SMSERR
+			resp["errno"] = utils.RecodeText(utils.RECODE_SMSERR)
+			fmt.Println("发送短信错误 _err", _err)
+		} else {
+			// 发送短信成功
+			resp["errno"] = utils.RECODE_OK
+			resp["errmg"] = utils.RecodeText(utils.RECODE_OK)
+			fmt.Println("发送短信成功")
+		}
+
+	} else {
+		// 校验失败 发送错误信息
+		resp["errno"] = utils.RECODE_CHECKERR
+		resp["errno"] = utils.RecodeText(utils.RECODE_CHECKERR)
+		fmt.Println("校验失败， 发送短信错误")
+	}
+
+	// 发送成功或者失败的 结果
+	ctx.JSON(http.StatusOK, resp)
 
 }
