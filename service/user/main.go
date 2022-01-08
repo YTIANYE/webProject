9d2d7754cf9770c1ce8665d2c674aeae37ea133a
yty <13293209977@163.com>
2022-01-08 14:16:11 +0800

    修改短信验证码移植过程中的redis操作相关问题

--- service/user/main.go
+++ service/user/main.go
@@ -5,19 +5,23 @@ import (
 	"github.com/micro/go-micro/util/log"
 	"github.com/micro/go-plugins/registry/consul"
 	"webProject/service/user/handler"
+	"webProject/service/user/model"
 	user "webProject/service/user/proto/user"
 )
 
 func main() {
+	// 初始化 Redis 连接池
+	model.InitRedis()
+
 	// 初始化consul
 	consulReg := consul.NewRegistry()
 
 	// New Service   ---   指定consul
 	service := micro.NewService(
 
-		micro.Address("192.168.17.129:12341"),
+		micro.Address("192.168.17.129:12341"), // 指定固定端口
 		micro.Name("go.micro.srv.user"),
-		micro.Registry(consulReg),
+		micro.Registry(consulReg), //注册服务
 		micro.Version("latest"),
 	)
 
